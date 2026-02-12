61db20056f910f5ee2f608f217b95def5cb55da2
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-02-11 20:52:09 -0500

    Fondo blanco

--- app/src/main/java/com/example/buy_it/ui/utils/backgrounds.kt
+++ app/src/main/java/com/example/buy_it/ui/utils/backgrounds.kt
@@ -1,2 +1,26 @@
 package com.example.buy_it.ui.utils
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.buy_it.R
+
+@Composable
+fun FondoBlanco(modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.bgwhite))
+    )
+
+}
+
+@Composable
+@Preview()
+fun FondoBlancoPreview(){
+    FondoBlanco()
+}
